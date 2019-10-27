6a7870e1dc40a350d6a0f9e33d22fc64a0c34e48
Ollenka <Ollenka@gmail.com>
2019-10-27 21:30:25 +0300

    Broke down into components. Install Dependencies - rect-redux, react-router-dom, redux-thunk, react

---
+++ src/components/MainContent/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MainContent.module.css';
+
+function MainContent() {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+}
+
+export default MainContent;
