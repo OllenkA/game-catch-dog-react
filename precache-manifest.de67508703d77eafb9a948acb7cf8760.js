c6b6a22c65024d33224759d5f4769c11a0ec7b6d
Ollenka <Ollenka@gmail.com>
2019-10-28 23:04:21 +0300

    Updates

---
+++ precache-manifest.de67508703d77eafb9a948acb7cf8760.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ea13bc1d83dff449b0211af7af13c1",
+    "url": "/game-catch-dog-react/index.html"
+  },
+  {
+    "revision": "4632da06dd629a3d4873",
+    "url": "/game-catch-dog-react/static/css/main.b474c601.chunk.css"
+  },
+  {
+    "revision": "e2757924fe841ec93a26",
+    "url": "/game-catch-dog-react/static/js/2.ea62f8e7.chunk.js"
+  },
+  {
+    "revision": "4632da06dd629a3d4873",
+    "url": "/game-catch-dog-react/static/js/main.16ac70cb.chunk.js"
+  },
+  {
+    "revision": "cb8641bd6aab1d2cb0e4",
+    "url": "/game-catch-dog-react/static/js/runtime-main.63b67089.js"
+  },
+  {
+    "revision": "21374653ae508ba4344c54203a338fb0",
+    "url": "/game-catch-dog-react/static/media/Dog.21374653.jpg"
+  },
+  {
+    "revision": "2ef44bc1626caadcf87c672bc900fc1b",
+    "url": "/game-catch-dog-react/static/media/dogs.2ef44bc1.mp3"
+  }
+]);
